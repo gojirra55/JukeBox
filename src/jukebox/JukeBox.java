354c66e016dde37cc505241e3affbf393cd96c1f
Jason Newman <gojirra@gmail.com>
2017-04-08 10:47:49 -0700

    Started implementing main menu.

--- src/jukebox/JukeBox.java
+++ src/jukebox/JukeBox.java
@@ -5,17 +5,47 @@
  */
 package jukebox;
 
+import java.util.Scanner;
+
+
 /**
  *
  * @author Jason
  */
 public class JukeBox {
 
-    /**
-     * @param args the command line arguments
-     */
+    private static double currentBalance = 0;
+    private static Scanner scanner = new Scanner(System.in);
+    
+    public static double insertMoney (double moneyInserted)
+    {
+        currentBalance += moneyInserted;
+        return currentBalance;
+    }
+            
     public static void main(String[] args) {
+     
+        //Display main menu.
+        System.out.println("1) Insert Money");
+        System.out.println("2) Search");
+        System.out.println("3) Play Random");
+        System.out.println("Enter a choice (1 - 3):");
+        int input = scanner.nextInt();
         
+        switch (input)
+        {
+            case 1: System.out.println("Please enter the amount of money to insert: ");
+                    double moneyInserted = scanner.nextDouble();
+                    insertMoney(moneyInserted);
+                    break;
+            
+            case 2: //search();
+                    break;
+                    
+            case 3: //playRandom();
+                    break;
+            
+        }
     }
     
 }
